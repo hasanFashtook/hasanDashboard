675628d3257968fb8dfda2b28ffb98a81f31553b
hasan <hasanfashtook5000@gmail.com>
2023-04-29 19:58:28 +0300

    Add your commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let checkbox1 = document.getElementById("123");
+let checkbox2 = document.getElementById("1234");
+let toggleSwitch = document.querySelectorAll(".toggle-switch")[0];
+let label = document.getElementsByTagName("label")[0];
+let table = document.querySelector(".Projects-table-widget");
+let social = document.querySelector(".Social-Media-Stats")
+// let btn = document.querySelector(".Projects-table-widget table tbody tr td button");
+// console.log(btn);
+
+// toggleSwitch.addEventListener("click",() => {
+//   toggleSwitch.classList.toggle("checked")
+//   checkbox.toggleAttribute("checked")
+// })
+// btn.addEventListener("click", () => {
+//   btn.parentElement.parentElement.remove();
+// })
+document.addEventListener("scroll", () => {
+  console.log(table.offsetTop)
+  console.log(table.clientHeight)
+  console.log(social.offsetTop)
+  console.log(social.clientHeight)
+})
