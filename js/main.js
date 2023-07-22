64388b2b1691688203692542874a7bf58d2140f8
hasanFashtouk <hasanfashtook5000@gmail.com>
2023-07-22 08:24:33 +0300

    add dark mode to index.html

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-// let checkbox1 = document.getElementById("123");
-// let checkbox2 = document.getElementById("1234");
-// let toggleSwitch = document.querySelectorAll(".toggle-switch")[0];
-// let label = document.getElementsByTagName("label")[0];
-// let table = document.querySelector(".Projects-table-widget");
-// let social = document.querySelector(".Social-Media-Stats")
-// let btn = document.querySelector(".Projects-table-widget table tbody tr td button");
-// console.log(btn);
+let modeBtn = document.getElementById("mode");
+let iconsMode = document.querySelectorAll("input.toggle-on-off + label .circle i");
+let mode;
 
-// toggleSwitch.addEventListener("click",() => {
-//   toggleSwitch.classList.toggle("checked")
-//   checkbox.toggleAttribute("checked")
-// })
-// btn.addEventListener("click", () => {
-//   btn.parentElement.parentElement.remove();
-// })
+modeBtn.addEventListener("click",()=>{
+  iconsMode.forEach((icon)=>{
+    icon.classList.toggle("hidden")
+    if(!icon.classList.contains("hidden")){
+      mode = icon.classList.item(1).slice(3); // start with 3 index to hide fa- form classes
+    } 
+  })
+  toggleMode(mode);
+})
+
+function toggleMode(mode){
+  if(mode == "moon"){
+    document.documentElement.style.setProperty('--primary-color', '#1f1f1f');
+    document.documentElement.style.setProperty('--secondary-color', '#282828');
+    document.documentElement.style.setProperty('--tertiary-color', '#424242');
+    document.documentElement.style.setProperty('--primary-font-color', '#fff');
+    document.body.style.color = 'white'
+  }else{
+    document.documentElement.style.setProperty('--primary-color', '#f5f9ff');
+    document.documentElement.style.setProperty('--secondary-color', '#fff');
+    document.documentElement.style.setProperty('--tertiary-color', '#edecff');
+    document.documentElement.style.setProperty('--primary-font-color', '#000');
+    document.body.style.color = 'black'
+  }
+}
