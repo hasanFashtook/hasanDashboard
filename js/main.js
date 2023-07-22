695c0fb93432898f6b50bacb578ba5c776bed88c
hasanFashtouk <hasanfashtook5000@gmail.com>
2023-07-23 01:21:03 +0300

    add dark mode feature to all pages

--- js/main.js
+++ js/main.js
@@ -2,24 +2,40 @@ let modeBtn = document.getElementById("mode");
 let iconsMode = document.querySelectorAll("input.toggle-on-off + label .circle i");
 let mode;
 
-modeBtn.addEventListener("click",()=>{
-  iconsMode.forEach((icon)=>{
+
+if (localStorage.getItem('mode')) {
+  mode = localStorage.getItem('mode');
+  if (mode == 'moon') {
+    iconsMode.forEach((icon) => {
+      icon.classList.toggle('hidden')
+    })
+    modeBtn.setAttribute('checked', '');
+  } else {
+    modeBtn.removeAttribute('checked');
+  }
+  toggleMode(mode);
+}
+
+modeBtn.addEventListener("click", () => {
+  iconsMode.forEach((icon) => {
     icon.classList.toggle("hidden")
-    if(!icon.classList.contains("hidden")){
+    if (!icon.classList.contains("hidden")) {
       mode = icon.classList.item(1).slice(3); // start with 3 index to hide fa- form classes
-    } 
+    }
   })
+  localStorage.setItem('mode', mode)
   toggleMode(mode);
 })
 
-function toggleMode(mode){
-  if(mode == "moon"){
+
+function toggleMode(mode) {
+  if (mode == "moon") {
     document.documentElement.style.setProperty('--primary-color', '#1f1f1f');
     document.documentElement.style.setProperty('--secondary-color', '#282828');
     document.documentElement.style.setProperty('--tertiary-color', '#424242');
     document.documentElement.style.setProperty('--primary-font-color', '#fff');
     document.body.style.color = 'white'
-  }else{
+  } else {
     document.documentElement.style.setProperty('--primary-color', '#f5f9ff');
     document.documentElement.style.setProperty('--secondary-color', '#fff');
     document.documentElement.style.setProperty('--tertiary-color', '#edecff');
